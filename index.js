c3aa417f7947e9933ea07a4aeb8aca5b24e044e1
bswiley <bswiley@gmail.com>
2023-05-12 02:35:34 -0500

    fixed some glitches

--- index.js
+++ index.js
@@ -4,41 +4,39 @@ const inquirer = require("inquirer");
 
 
 
-// inquirer.prompt([
-// {
-// type: 'list',
-// message: 'What kind of shape would you like the letters to be shown inside of?',
-// name: 'shap',
-// choices: ["Circle","Triangle","Square"]
-// },
+inquirer
+.prompt([
+{
+type: 'list',
+message: 'What kind of shape would you like the letters to be shown inside of?',
+name: 'shape',
+choices: ["Circle","Triangle","Square"]
+},
 
-// {
-// type: 'input',
-// message: 'What letters would you like to use in your logo? (You can choose up to three letters; case sensitive)',
-// name: 'text',
-// },
+{
+type: 'input',
+message: 'What letters would you like to use in your logo? (You can choose up to three letters; case sensitive)',
+name: 'text',
+},
 
-// {  
-// type: 'input',
-// message: 'What color would you like your chosen shape to be? (it can be a popular color name or a hexadecimal color value)',
-// name: 'sColo',
-// },
+{  
+type: 'input',
+message: 'What color would you like your chosen shape to be? (it can be a popular color name or a hexadecimal color value)',
+name: 'sColor',
+},
 
-// {
-// type: 'input',
-// message: "What color would you like the text to be in?(it can be a popular color name or a hexadecimal color value)",
-// name: 'tColo',
-// }
-// ])
-//     .then((response)=>{
-//     console.log(response)
-//     const {text, shape, sColor, tColor} = response;
-var text = "BSW"
-var shape = "square"
-var sColor = "#1b54b5"
-var tColor = "#ed6f7c"
+{
+type: 'input',
+message: "What color would you like the text to be in?(it can be a popular color name or a hexadecimal color value)",
+name: 'tColor',
+}
+])
+.then((response)=>{
+console.log(response)
+const {shape, text, sColor, tColor} = response;
 writeSVG(text, shape, sColor, tColor);
-// })
+})
+
 
 
 function writeSVG(text, shap, sColo, tColo){
@@ -46,42 +44,36 @@ function writeSVG(text, shap, sColo, tColo){
    let sColor = sColo.toLowerCase();
    let tColor = tColo.toLowerCase();
    console.log (shape, sColor, tColor)
-}
+
 
 fs.writeFile(`./examples/${shape}.svg`,'<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n', err  =>
 err ? console.error(err): console.log ("SVG started"));
 
+addShape(shape,sColor,tColor,text);
+}
+
+function addShape (shape,sColor,tColor,text){
+
 switch (shape){
 
 case 'square':
-let sFigure = new Square(sColor)
-console.log(sFigure.render());
+let sFigure = new Square(sColor, tColor, text)
 
 fs.appendFile(`./examples/${shape}.svg`, sFigure.render() , err  =>
 err ? console.error(err): console.log ("square written"));
-
-fs.appendFile(`./examples/${shape}.svg`, `<text x="150" y="125" font-size ="60" text-anchor="middle" fill="${tColor}">${text}</text>\n </svg>\n` , err  =>
-err ? console.error(err): console.log ("text written"));
 break;
 
 case "circle":
-let cFigure = new Circle(sColor)   
+let cFigure = new Circle(sColor, tColor, text)  
 
 fs.appendFile(`./examples/${shape}.svg`, cFigure.render(), err  =>
 err ? console.error(err): console.log ("circle written"));
-
-fs.appendFile(`./examples/${shape}.svg`, `<text x="150" y="125" font-size ="60" text-anchor="middle" fill="${tColor}">${text}</text>\n </svg>\n` , err  =>
-err ? console.error(err): console.log ("text written"));
 break;
 
 case "triangle":
-let tFigure = new Triangle(sColor)   
+let tFigure = new Triangle(sColor, tColor, text)  
 
-fs.appendFile(`./examples/${shape}.svg`, tFigure.render(sColor), err  =>
+fs.appendFile(`./examples/${shape}.svg`, tFigure.render(), err  =>
 err ? console.error(err): console.log ("triangle written"));
-
-fs.appendFile(`./examples/${shape}.svg`, `<text x="150" y="150" font-size ="60" text-anchor="middle" fill="${tColor}">${text}</text>\n </svg>\n` , err  =>
-err ? console.error(err): console.log ("text written"));
 break;
-}
-
+}}
